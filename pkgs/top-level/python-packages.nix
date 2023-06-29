a4510bc641174782341cc0e9f42be4bf21713b54
Fabian Affolter <mail@fabian-affolter.ch>
2023-06-29 20:21:53 +0200

    python311Packages.google-ai-generativelanguage: init at 0.3.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -4187,6 +4187,8 @@ self: super: with self; {
 
   goodwe = callPackage ../development/python-modules/goodwe { };
 
+  google-ai-generativelanguage = callPackage ../development/python-modules/google-ai-generativelanguage { };
+
   google-api-core = callPackage ../development/python-modules/google-api-core { };
 
   google-api-python-client = callPackage ../development/python-modules/google-api-python-client { };
