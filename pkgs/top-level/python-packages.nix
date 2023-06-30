1dfa9630d2f0f01a310c0a1d84c185973c882f6e
gabriel-doriath-dohler <gabriel.doriath.dohler@ens.fr>
2023-06-30 23:59:59 +0000

    python3Packages.nbtlib: init at 2.0.4

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -6726,6 +6726,8 @@ self: super: with self; {
 
   nbsphinx = callPackage ../development/python-modules/nbsphinx { };
 
+  nbtlib = callPackage ../development/python-modules/nbtlib { };
+
   nbval = callPackage ../development/python-modules/nbval { };
 
   nbxmpp = callPackage ../development/python-modules/nbxmpp { };
