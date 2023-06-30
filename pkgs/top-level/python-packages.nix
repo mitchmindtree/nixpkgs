da691cf6fbee651e69ab977629d23c7eb17899eb
gabriel-doriath-dohler <gabriel.doriath.dohler@ens.fr>
2023-06-30 17:10:13 +0200

    python3Packages.litemapy: init at 0.7.2b0
    Co-authored-by: Ryan Lahfa <masterancpp@gmail.com>

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5894,6 +5894,8 @@ self: super: with self; {
 
   lit = callPackage ../development/python-modules/lit { };
 
+  litemapy = callPackage ../development/python-modules/litemapy { };
+
   littleutils = callPackage ../development/python-modules/littleutils { };
 
   livelossplot = callPackage ../development/python-modules/livelossplot { };
